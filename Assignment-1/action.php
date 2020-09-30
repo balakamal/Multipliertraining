f82aee8d54361caa8d22db18bc834cc09c57b6fe
balakamal <balakamal22@gmail.com>
2020-09-30 20:09:25 +0530

    first commit

---
+++ Assignment-1/action.php
@@ -0,0 +1,14 @@
+
+<?php 
+    $uname = $_POST["uname"]; 
+    $pass = $_POST["pass"];
+    if($pass == 'password')
+    {
+        session_start();
+        echo("<h2>Session started</h2>");
+    }
+    else{
+        header("Location:index.html");
+        exit();
+    } 
+?>
