58a4b695aa6bc1a1ec089147f6c0d7d395fad167
balakamal <balakamal22@gmail.com>
2020-10-02 17:48:18 +0530

    commit

--- Assignment-2/index.php
+++ Assignment-2/index.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8">
     <title>Insert and Retrieve data from MySQL database</title>
     <link rel="stylesheet" href="styles.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="scripts.js"></script>
     </head>
     <body>
     <div class="wrapper">
@@ -24,6 +22,7 @@
         </form>
     </div>
     </body>
-    
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="scripts.js"></script>
 </html>
     
