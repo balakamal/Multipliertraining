1c5c96709e8a89b254735fb1670b8ca1d6365228
balakamal <balakamal22@gmail.com>
2020-10-07 12:06:41 +0530

    commit

---
+++ Assignment-4/index.php
@@ -0,0 +1,28 @@
+<?php include('server.php'); ?>
+<!DOCTYPE html>
+    <head>
+    <meta charset="UTF-8">
+    <title>Insert and Retrieve data from MySQL database</title>
+    <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+    <div class="wrapper">
+        <?php echo $comments; ?>
+        <form class="comment_form">
+        <div>
+            <label for="name">Name:</label>
+            <input type="text" name="name" id="name">
+        </div>
+        <div>
+            <label for="comment">Comment:</label>
+            <textarea name="comment" id="comment" cols="30" rows="5"></textarea>
+        </div>
+        <button type="button" id="submit_btn">POST</button>
+        <button type="button" id="update_btn" style="display: none;">UPDATE</button>
+        </form>
+    </div>
+    </body>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="scripts.js"></script>
+</html>
+    
